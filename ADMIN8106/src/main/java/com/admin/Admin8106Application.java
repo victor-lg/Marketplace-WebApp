c18c864c684132aaa5050f0db38572c9bcaaf596
alejandrori124 <71637057+alejandrori124@users.noreply.github.com>
2021-11-25 18:30:24 +0100

    change admin to spring

---
+++ ADMIN8106/src/main/java/com/admin/Admin8106Application.java
@@ -0,0 +1,13 @@
+package com.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Admin8106Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Admin8106Application.class, args);
+	}
+
+}
